a18d99eb28d6b5f679802bc3ec1912d121ac1e5a
Anya Vigolajnen <avi26.job@yandex.ru>
2018-06-07 23:25:16 +0300

    Изменяет название массивов

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,12 @@
 'use strict';
 
 document.querySelector('.setup').classList.remove('hidden');
-var similarListElement = document.querySelector('.setup-similar-list');
-document.querySelector('.setup-similar').classList.remove('hidden');
 
-var FIRSTNANE = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var QUANTITY_WIZARDS = 4;
-
 
 var getRandomValue = function (array) {
   var rand = Math.floor(Math.random() * array.length);
@@ -18,9 +15,9 @@ var getRandomValue = function (array) {
 
 var createWizard = function () {
   return {
-    name: getRandomValue(FIRSTNANE).concat(' ').concat(getRandomValue(SURNAME)),
-    coatColor: getRandomValue(COAT_COLOR),
-    eyesColor: getRandomValue(EYES_COLOR)
+    name: getRandomValue(firstNames).concat(' ').concat(getRandomValue(surnames)),
+    coatColor: getRandomValue(coatColors),
+    eyesColor: getRandomValue(eyesColors)
   };
 };
 
@@ -35,8 +32,11 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
+var similarListElement = document.querySelector('.setup-similar-list');
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < QUANTITY_WIZARDS; i++) {
   fragment.appendChild(renderWizard(createWizard()));
 }
 similarListElement.appendChild(fragment);
+
+document.querySelector('.setup-similar').classList.remove('hidden');
