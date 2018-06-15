b62a77bb244c357e9e1dd3359f504d53ad6fe4b4
Anna Vigolajnen <vigolajnen@gmail.com>
2018-06-15 09:53:25 +0300

    Добавляет параметр для функции renderWizard

--- js/setup.js
+++ js/setup.js
@@ -38,17 +38,17 @@ for (var j = 0; j < QUANTITY_WIZARDS; j++) {
   wizards.push(createWizard());
 }
 
-var renderWizard = function () {
+var renderWizard = function (wizard) {
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < QUANTITY_WIZARDS; i++) {
-  fragment.appendChild(renderWizard(createWizard()));
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
