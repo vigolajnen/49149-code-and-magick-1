31df43337b6562218185d3aa44b8370857ebc26c
Anya Vigolajnen <avi26.job@yandex.ru>
2018-06-16 01:45:45 +0300

    задание - одеть Надежду - начало

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
+//document.querySelector('.setup').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
@@ -52,3 +52,73 @@ for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
+
+// Открытие/закрытие окна настройки персонажа:
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+var coatColorInput = document.querySelector('input[name=coat-color]');
+var eyesColorInput = document.querySelector('input[name=eyes-color]');
+var fireballColorInput = document.querySelector('input[name=fireball-color]');
+var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+// Изменение цвета мантии персонажа по нажатию.
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getRandomValue(coatColors);
+  coatColorInput.value = wizardCoat.style.fill;
+});
+
+// Изменение цвета глаз персонажа по нажатию.
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getRandomValue(eyesColors);
+  eyesColorInput.value = wizardEyes.style.fill;
+});
+
+// Изменение цвета фаерболов по нажатию.
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.background = getRandomValue(fireballColor);
+  fireballColorInput.value = wizardFireball.style.background;
+});
